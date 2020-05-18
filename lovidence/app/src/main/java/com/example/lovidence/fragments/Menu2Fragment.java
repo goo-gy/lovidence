321614a10f455f67258cc2dde923f3705e2b0150
taehjoon <zxc8283@daum.net>
2020-05-18 16:46:28 +0900

    도시명으로 바꿔서 그래프로 나타냈음 그런데 layout을 모르겠네... ㅠ

--- lovidence/app/src/main/java/com/example/lovidence/fragments/Menu2Fragment.java
+++ lovidence/app/src/main/java/com/example/lovidence/fragments/Menu2Fragment.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Geocoder;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -25,15 +26,26 @@ import com.example.lovidence.R;
 import com.example.lovidence.SQLite.Couple_Location;
 import com.example.lovidence.SQLite.Couple_LocationDao;
 import com.example.lovidence.SQLite.MyDatabase;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class Menu2Fragment extends Fragment {
-    private Button createQRBtn;
-    private Button scanQRBtn;
-    private TextView DB_content;
+    PieChart pieChart; //원형차트
     private getAsyncTask getDB;
     private MyDatabase db;
 
@@ -48,15 +60,16 @@ public class Menu2Fragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_menu2, container, false);
         db = MyDatabase.getAppDatabase(getActivity());
         getDB = new getAsyncTask(getActivity(),db.todoDao());
+        pieChart= view.findViewById(R.id.piechart);
 
-        DB_content = view.findViewById(R.id.showsDB);
+
         try {
-            DB_content.setText(getDB.execute().get());
+            setPieChart(getDB.execute().get());
         }catch (Exception e){e.printStackTrace();}
 
         return view;
     }
-    public static class getAsyncTask extends AsyncTask<Void, Void, String> {
+    public static class getAsyncTask extends AsyncTask<Void, Void, ArrayList<String>> {
         private Couple_LocationDao mTodoDao;
         private Context context;
 
@@ -66,17 +79,55 @@ public class Menu2Fragment extends Fragment {
         }
 
         @Override //백그라운드작업(메인스레드 X)
-        protected String doInBackground(Void ...voids) {
+        protected ArrayList<String> doInBackground(Void ...voids) {
             Log.e("??",mTodoDao.getAll().toString());
+            ArrayList<String> locations = new ArrayList<String>();
             Geocoder geo;
             geo = new Geocoder(context, Locale.getDefault());
-            String result="";
+
             for(int i=0; i<mTodoDao.getAll().size(); i++){
                 Couple_Location location = mTodoDao.getAll().get(i);
-                try{result +=geo.getFromLocation(location.getLocationX(),location.getLocationY(),1).get(0).getAdminArea();result +="\n";}
+                try{locations.add(geo.getFromLocation(location.getLocationX(),location.getLocationY(),1).get(0).getAdminArea());}
                 catch (Exception e){Log.e(Double.toString(location.getLocationX()),Double.toString(location.getLocationX()));}
             }
-            return result;
+            return locations;
         }
+    }
+    void setPieChart(ArrayList<String> locations){
+        pieChart.setUsePercentValues(true);             //percent사용
+        pieChart.getDescription().setEnabled(false);    //descript사용 안함(default는 사용, 왼쪽하단면에 설명표시)
+        pieChart.setExtraOffsets(5,10,5,5); //offset
+
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+
+        pieChart.setDrawHoleEnabled(true);             //도넛형으로 만들지? ㄴㄴ
+        pieChart.setHoleColor(Color.WHITE);             //도넛형일경우 중간 빈공간 하얀색
+        pieChart.setTransparentCircleRadius(61f);       //그리고 공간크기
+
+        ArrayList<PieEntry> yValues = new ArrayList<PieEntry>();    //차트의 값의 array
+        ArrayList<String> distinct = Lists.newArrayList(Sets.newHashSet(locations));
+        for(String e: distinct){
+            int count = Collections.frequency(locations,e);
+            yValues.add(new PieEntry(count,e));
+        }
+
+        Description description = new Description();
+        description.setText("세계 국가"); //라벨
+        description.setTextSize(15);
+        pieChart.setDescription(description);
+
+        //pieChart.animateY(1000, Easing.EaseInOutCubic); //애니메이션
+
+        PieDataSet dataSet = new PieDataSet(yValues,"Countries");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        PieData data = new PieData((dataSet));
+        data.setValueTextSize(10f);
+        data.setValueTextColor(Color.YELLOW);
+
+        pieChart.setData(data);
+
     }
 }
