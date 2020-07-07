350b5b58014dcbb2c54c1ff905fddc5950fb738e
goo-gy <lkukh17@naver.com>
2020-07-07 17:27:07 +0900

    fix fragment4
    fix fragment4 ( only landmark page)

--- lovidence/app/src/main/java/com/example/lovidence/fragments/Menu4Fragment.java
+++ lovidence/app/src/main/java/com/example/lovidence/fragments/Menu4Fragment.java
@@ -33,13 +33,7 @@ import java.util.Calendar;
 public class Menu4Fragment extends Fragment {
     ViewGroup viewGroup;
     Context menu4_context;
-    Button button1;
-    Button logoutBtn;
-    Button mathingBtn;
-    Button deleteBtn;
-    Button LandmarkBtn;
     WorkManager workManager;
-
 
     @Nullable
     @Override
@@ -47,105 +41,10 @@ public class Menu4Fragment extends Fragment {
         menu4_context = getActivity();
         viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_menu4, container, false);
 
-        button1 = (Button) viewGroup.findViewById(R.id.btn1);
-        logoutBtn = (Button) viewGroup.findViewById(R.id.logout);
-        mathingBtn = (Button) viewGroup.findViewById(R.id.mtBtn);
-        deleteBtn = (Button) viewGroup.findViewById(R.id.deleteAccount);
-        LandmarkBtn = (Button) viewGroup.findViewById(R.id.LMBtn);
-        //예약된 work 모두삭제
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (workManager != null) {
-                    Log.e("hello?", "hell?");
-                    workManager.cancelAllWork();
-                    Toast.makeText(getContext(), "all task deleted...", Toast.LENGTH_SHORT);
-                }
-            }
-        });
-        logoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LogOutAction();
-            }
-        });
-        LandmarkBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(menu4_context, LandmarkActivity.class);
-                startActivity(intent);
-                getActivity().finish();
-            }
-        });
-        mathingBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(menu4_context, Matching.class);
-                startActivity(intent);
-                getActivity().finish();
-            }
-        });
-        deleteBtn.setOnClickListener(new View.OnClickListener() {
-            AlertDialog.Builder builder
-                    = new AlertDialog.Builder(getActivity());
-            boolean confirmDelete = false;
-
-            @Override
-            public void onClick(View view) {
-                builder.setTitle("계정삭제")
-                        .setMessage("계정을 삭제하시겠습니까?")
-                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                String data = "";
-                                PostAsync deleteAsync = new PostAsync();
-                                SharedPreferences sharedPref = getActivity().getSharedPreferences("USERINFO", Context.MODE_PRIVATE);
-                                String id = sharedPref.getString("USERID", "");
-                                if (id.equals("")) {
-                                    Log.e("Error", "user id is not stored");
-                                }
-                                try {
-                                    data = URLEncoder.encode("u_id", "UTF-8") + "=" + URLEncoder.encode(id, "UTF-8");
-
-                                    String result = deleteAsync.execute("userDelete.php", data).get();
-                                    if (result.equals("success")) {
-                                        Log.e("삭제성공", "success");
-                                        Toast.makeText(getActivity(), "삭제완료", Toast.LENGTH_SHORT).show();
-                                        LogOutAction();
-                                    } else {
-                                        Log.e("삭제실패?", result);
-                                        Toast.makeText(getActivity(), "삭제실패", Toast.LENGTH_SHORT).show();
-                                        LogOutAction();
-                                    }
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        })
-                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-
-                            }
-                        }).create().show();
-
-            }
-        });
-        return viewGroup;
-    }
-
-    private void LogOutAction() {
-        Intent intent = new Intent(getActivity(), SplashActivity.class);
+        Intent intent = new Intent(menu4_context, LandmarkActivity.class);
         startActivity(intent);
-        if (workManager != null) {
-            Log.e("hello?", "hell?");
-            workManager.cancelAllWork();
-            Toast.makeText(getContext(), "all task deleted...", Toast.LENGTH_SHORT);
-        }
-        SharedPreferences sharedPref = getActivity().getSharedPreferences("USERINFO", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.remove("USERID").commit();
-        editor.remove("date").commit();
         getActivity().finish();
+
+        return viewGroup;
     }
 }
