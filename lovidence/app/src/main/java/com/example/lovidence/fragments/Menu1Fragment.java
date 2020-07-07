b68634739bf379983f46f1d00cef1a04c61e167b
goo-gy <lkukh17@naver.com>
2020-07-07 14:09:44 +0900

    config_sidebar
    add config_sidebar

--- lovidence/app/src/main/java/com/example/lovidence/fragments/Menu1Fragment.java
+++ lovidence/app/src/main/java/com/example/lovidence/fragments/Menu1Fragment.java
@@ -1,6 +1,8 @@
 package com.example.lovidence.fragments;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,14 +11,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.work.WorkManager;
 
+import com.example.lovidence.MainActivity;
+import com.example.lovidence.Matching;
 import com.example.lovidence.PostAsync.PostAsync;
 import com.example.lovidence.R;
+import com.example.lovidence.splash.SplashActivity;
 
 import java.net.URLEncoder;
 import java.text.ParseException;
@@ -30,6 +38,8 @@ public class Menu1Fragment extends Fragment {
     public Calendar the_date;
     TextView text_date;
     SharedPreferences sharedPref;
+
+    WorkManager workManager;
     // ---------------------------------------- googy
     @Nullable
     @Override
@@ -40,6 +50,34 @@ public class Menu1Fragment extends Fragment {
         the_date = Calendar.getInstance();
         update();// update
 
+        // ---------------------------------------- config
+        Button btn_matching = (Button)view.findViewById(R.id.config_matching);
+        btn_matching.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Matching!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(menu1_context, Matching.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        Button btn_test = (Button)view.findViewById(R.id.config_logout);
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LogOutAction();
+            }
+        });
+
+        Button btn_delete = (Button)view.findViewById(R.id.config_delete_account);
+        btn_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Delete!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        // ---------------------------------------- googy
         return view;
     }
 
@@ -102,4 +140,19 @@ public class Menu1Fragment extends Fragment {
         return love_day;
     }
     // ---------------------------------------- googy
+    private void LogOutAction() {
+        Intent intent = new Intent(getActivity(), SplashActivity.class);
+        startActivity(intent);
+        if (workManager != null) {
+            Log.e("hello?", "hell?");
+            workManager.cancelAllWork();
+            Toast.makeText(getContext(), "all task deleted...", Toast.LENGTH_SHORT);
+        }
+        SharedPreferences sharedPref = getActivity().getSharedPreferences("USERINFO", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.remove("USERID").commit();
+        editor.remove("date").commit();
+        Toast.makeText(getContext(), "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+        getActivity().finish();
+    }
 }
